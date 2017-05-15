aaf305312862b252d95791adf9500cc3c083d867
cathiec <xiao.xu.cathiec@gmail.com>
2017-05-15 17:56:11 +0200

    201705151757

---
+++ IMPACT/src/setting.h
@@ -0,0 +1,10 @@
+#ifndef setting_h
+#define setting_h
+
+#define MAX_SYMBOL 20
+#define RENAME_SYMBOL "_new"
+
+#define BACK_STEP 1 // k
+#define CONTAINER_MODE 1    // 1 = Queue 2 = Stack
+
+#endif
